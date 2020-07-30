1fc215df2d3b52700cf5236ee561d2ee773181bf
castle.vrn <castle.vrn@gmail.com>
2020-07-30 22:18:27 +0300

    sort, description, 6 slider

--- app/helpers/complectation_helper.rb
+++ app/helpers/complectation_helper.rb
@@ -1,11 +1,20 @@
 module ComplectationHelper
+  
   def complectations_ complectations, list=[]
     complectations.each do |complectation|  
       list.push complectation.name unless list.include? complectation.name
     end
     list
   end
+
   def sort_complectations complectations, sorted = []
+    case @pricesort
+    when 'min'
+      complectations = complectations.sort_by(&:price_new)  
+    when 'max'
+      complectations = complectations.sort_by(&:price_new).reverse
+    end
+
     complectations.each do |complectation|
       if complectation.price_new.to_i >= @pricemin and complectation.price_new.to_i <= @pricemax
         sorted.push complectation
@@ -13,4 +22,5 @@ module ComplectationHelper
     end
     sorted
   end
+
 end
