0a6d03f2a78f47df9d4a38c9c8e76ce9c8dba8d9
castle.vrn <castle.vrn@gmail.com>
2020-09-16 10:19:57 +0300

    ck edit

--- app/models/service/sale.rb
+++ app/models/service/sale.rb
@@ -3,4 +3,10 @@ class Service::Sale < ApplicationRecord
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif", "image/svg+xml"]
   has_attached_file :preview
   validates_attachment_content_type :preview, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif", "image/svg+xml"]
+  
+  rails_admin do
+    include_all_fields
+    exclude_fields  :created_at, :updated_at
+    field :description, :ck_editor #, :ckeditor => { :toolbar => 'mini' } 
+  end
 end
